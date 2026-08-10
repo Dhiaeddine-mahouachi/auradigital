318179b9d4d8b6704e7de9fee80389a27a5b1710
dhiaeddine mahouachi <dhiamah206@gmail.com>
2026-08-11 01:41:49 +0300

    Serve QuickSite v2 from AuraDigital worker

---
+++ src/worker-entry.js
@@ -0,0 +1,12 @@
+import app from './worker.js';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if ((url.pathname === '/quicksite' || url.pathname === '/quicksite/') && request.method === 'GET') {
+      const assetUrl = new URL('/quicksite-v2.html', url);
+      return env.ASSETS.fetch(new Request(assetUrl, request));
+    }
+    return app.fetch(request, env, ctx);
+  },
+};
